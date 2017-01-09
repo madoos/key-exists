ea1bf664d0f4f09ccfc25ad1c5818b1aef364784
madoos <maurice.ronet.dominguez@gmail.com>
2017-01-09 23:49:03 +0100

    implemented basic functionality

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 'use strict'
 
+const keyExists = require('./lib/keyExists')
+
+const res = keyExists({a: {b: [1, 2, 3, {c: [[0, 1]]}]}}, 'a.b[3].c[0][25]')
+
+console.log(res, '********************')
+// module.exports = keyExists
+
