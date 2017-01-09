170cd6533b08ec3fdaee4e2a9c57d5f48a0af7ee
madoos <maurice.ronet.dominguez@gmail.com>
2017-01-10 00:13:04 +0100

    fixed the error by observing the last key

--- index.js
+++ index.js
@@ -3,7 +3,4 @@
 const keyExists = require('./lib/keyExists')
 module.exports = keyExists
 
-// const res = keyExists({a: {b: [1, 2, 3, {c: [[0, 1]]}]}}, 'a.b[3].c[0][25]')
-// bconsole.log(res, '********************')
-
 
