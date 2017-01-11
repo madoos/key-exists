e80b80489d9bd4f34a853acd70f960e9faeec485
madoos <maurice.ronet.dominguez@gmail.com>
2017-01-11 23:39:44 +0100

    added more unit tests

--- test/index.js
+++ test/index.js
@@ -10,13 +10,101 @@ describe('Import keyExists', function () {
   })
 })
 
+describe('To check if a key "b" exists', function () {
+  it('should to be a false', function () {
+      let obj = {}
+      expect(keyExists(obj, 'b')).to.be.false
+  })
+})
+
 describe('To check if a key "b.c[0]" exists', function () {
   it('should to be a true', function () {
-      const test = {
+      let obj = {
         b:{
           c: [{}]
         }
       }
-      expect(keyExists(test, 'b.c[0]')).to.be.true
+      expect(keyExists(obj, 'b.c[0]')).to.be.true
+  })
+})
+
+describe('To check if a key "a[2].b[0]" exists', function () {
+  it('should to be a true', function () {
+      let obj = {
+        a: [
+          {},
+          {},
+          {b: [undefined]}
+        ]
+      }
+      expect(keyExists(obj, 'a[2].b[0]')).to.be.true
+  })
+})
+
+describe('To check if a key "a[2].b[1]" exists', function () {
+  it('should to be a false', function () {
+      let obj = {
+        a: [
+          {},
+          {},
+          {b: [undefined]}
+        ]
+      }
+      expect(keyExists(obj, 'a[2].b[1]')).to.be.false
+  })
+})
+
+describe('To check if a key "a[0].b" exists', function () {
+  it('should to be a true', function () {
+      let obj = {
+        a: [
+          {b: null},
+          {},
+          {b: [undefined]}
+        ]
+      }
+      expect(keyExists(obj, 'a[0].b')).to.be.true
+  })
+})
+
+describe('To check if a key "[0].a[1]" exists', function () {
+  it('should to be a true', function () {
+      let arr = [{a: [1, 2, 3]}]
+      expect(keyExists(arr, '[0].a[1]')).to.be.true
+  })
+})
+
+describe('To check if a key "[0].a[3]" exists', function () {
+  it('should to be a false', function () {
+      let arr = [{a: [1, 2, 3]}]
+      expect(keyExists(arr, '[0].a[3]')).to.be.false
+  })
+})
+
+describe('To check if a key "[0][0][0]" exists', function () {
+  it('should to be a true', function () {
+      let arr = [
+        [
+          [1, 2, 3],
+          [1, 2, 3]
+        ],
+        [],
+        []
+      ]
+      expect(keyExists(arr, '[0][0][1]')).to.be.true
+  })
+})
+
+describe('To check if a key "[0]" exists', function () {
+  it('should to be a true', function () {
+      let arr = [
+        [
+          null,
+          [1, 2, 3]
+        ],
+        [],
+        []
+      ]
+      expect(keyExists(arr, '[0]')).to.be.true
   })
 })
